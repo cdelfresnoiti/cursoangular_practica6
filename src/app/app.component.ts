d5a300cebf16576d10aad5d079bd62184204677d
Carlos del Fresno <carlos@delfresno.com>
2020-11-08 08:57:01 +0100

    Práctica 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'practicaLifeCycle';
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit,
+AfterViewChecked, DoCheck, AfterContentInit, AfterContentChecked {
+
+  ngOnInit() {
+    console.log('onInit()');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy()');
+  }
+
+  // ngOnChanges() {
+  //   console.log('ngOnChanges()');
+  // }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  test() {
+    
+  }
+
 }
